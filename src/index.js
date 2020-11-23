8c8fdb2f450af26fa99c0134987a9e1f2bb9cda8
Stacy Huang <stacy@stacys-mbp.lan>
2020-11-22 23:05:03 -0800

    Add basic webpack config

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "fontsource-patrick-hand-sc"
+import "./css/normalize.css";
+import "./sass/main.scss";
+import "./assets/profile-large.jpg";
+import "./assets/profile-medium.jpg";
+import "./assets/profile-small.jpg";
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  entry: [
+    './src/index.js',
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+}
